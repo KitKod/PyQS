562aeb761e719195fcc424ace6267e10b1b7e35e
Mykyta Kamak <kamaknikita@gmail.com>
2019-03-14 21:51:15 +0200

    Add a validation of the of count field value using a QMessageBox.

--- logic/builder.py
+++ logic/builder.py
@@ -1,3 +1,5 @@
+from PyQt5.QtWidgets import QMessageBox
+
 from .model import random_state
 
 
@@ -6,4 +8,10 @@ def build_plot(mui):
     min = int(mui.lineEdit_min.text())
     count = int(mui.lineEdit_count.text())
 
-    mui.mpl.build_bar(*random_state(min, max, count))
+    if count > 50:
+        msg_box = QMessageBox()
+        msg_box.setText("You have entered a very large value in the count field. \n"
+                        "The maximum possible value is 50.")
+        msg_box.exec()
+    else:
+        mui.mpl.build_bar(*random_state(min, max, count))
